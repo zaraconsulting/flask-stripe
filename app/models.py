044ec63264df2ba41c66ed77c472c0c6dc257270
derekcodingtemple <derek@codingtemple.com>
2019-09-10 10:08:40 -0500

    Changed databases

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db, admin
 from flask_admin import form
 from flask_admin.contrib.sqla import ModelView
+from sqlalchemy.dialects.postgresql import UUID
 
 class Product(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +13,14 @@ class Product(db.Model):
 
   def __repr__(self):
     return f'<Product: {self.name} | {self.price}>'
+
+class Coupon(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  uuid = db.Column(UUID(as_uuid=True), unique=True, nullable=False)
+  code = db.Column(db.String(10), nullable=False)
+
+  def __repr__(self):
+    return f'<self.code>'
 
 admin.add_views(
   ModelView(Product, db.session),
--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 
 class Config:
   SECRET_KEY = os.getenv('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'app.db')}"
+  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   MAIL_SERVER = os.getenv('MAIL_SERVER')
   MAIL_PORT = os.getenv('MAIL_PORT')
