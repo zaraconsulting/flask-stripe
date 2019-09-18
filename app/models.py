57e630dab874b046a0cce460293d4bae095cbb7a
derekcodingtemple <derek@codingtemple.com>
2019-09-18 13:23:23 -0500

    if product edited, stripe product edited as well

--- app/models.py
+++ app/models.py
@@ -77,21 +77,56 @@ class ProductView(ModelView):
       self.session.rollback()
       return False
     return self.render('admin/model/create.html', form=form)
+
+  def update_model(self, form, model):
+    try:
+      for i in stripe.SKU.list():
+        if i.product == model.id_:
+          sid = i.id
+
+      # Update Stripe SKU information
+      sku = stripe.SKU.modify(
+        sid,
+        image = form.image.data,
+        price = int(form.price.data)*100
+      )
+
+      # Update Stripe Product information
+      product = stripe.Product.modify(
+        model.id_,
+        name = form.name.data,
+        images = [sku.image],
+        description = form.description.data
+      )
+
+      # Update database Product model
+      model.name = product.name
+      model.image = sku.image
+      model.price = sku.price
+      model.description = product.description
+      db.session.commit()
+    
+    except Exception as ex:
+      if not self.handle_view_exception(ex):
+        flash(gettext('Failed to update record. %(error)s', error=str(ex)), 'error')
+        log.exception('Failed to update record.')
+      self.session.rollback()
+      return False
+    return True
+
   
   def delete_model(self, model):
     try:
-      p = Product.query.filter_by(id_=model.id_).first()
-
       # Delete SKU from Stripe (must be done before deleting product)
       for i in stripe.SKU.list():
-        if i.product == p.id_:
+        if i.product == model.id_:
           stripe.SKU.delete(i.id)
       
       # Delete product from Stripe
-      stripe.Product.delete(p.id_)
+      stripe.Product.delete(model.id_)
 
       # Delete from database
-      db.session.delete(p)
+      db.session.delete(model)
       db.session.commit()
 
     except Exception as ex:
